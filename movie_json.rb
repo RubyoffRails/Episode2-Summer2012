6062d0020715554ac1d708434c96450cb0a14be3
James Levinson <jamlevi@comcast.net>
2012-09-21 17:29:33 -0700

    tiger?

--- movie_json.rb
+++ movie_json.rb
@@ -1,24 +1,33 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
+@@movies = []
+
 def find_movie
-  puts "OH HAI. Search?"
+  puts "Add a movie you really like:"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
-  if movie.id.eql?(0)
-    puts "Movie Not Found"
-  else
-    puts "Found: #{movie.title}. Score: #{movie.score}"
+  @@movies << movie
+  puts "Added #{movie.title} with a rating of #{movie.score}"
+end
+
+def compute_average
+  total = 0.0
+  @@movies.each do |m|
+    total += m.score
   end
+  average = total / @@movies.size.to_f
 end
 
 find_movie
-
 while true do
-  puts "Search Again (Y/N)" 
+  puts "Add Another (Y) or Compute Average? (A)"
   answer = gets.upcase[0]
   if answer == "Y"
     find_movie
+  elsif answer == "A"
+    puts "The average score of the movies you like is: #{compute_average}"
+    break
   else
     break
   end
