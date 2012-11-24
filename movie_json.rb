2f03808f549573f9276d5f4971764a22dd53c8ec
J. Michael Kasiewicz <crestcode@gmail.com>
2012-11-24 12:32:40 -0500

    Show average year of movies and determine user temperament based on searched movie stats

--- movie_json.rb
+++ movie_json.rb
@@ -2,23 +2,42 @@ require_relative "lib/movie"
 require_relative "lib/api"
 
 def find_movie
-  @movies = [] if @movies == nil
   puts 'Add a movie you really like:'
   movie_title = gets
   if !(current_movie = Api.search_by_title(movie_title))
     puts 'Oops. Something went wrong or title not found.'
   else
     @movies << current_movie
-    puts "Found: #{@movies.last.title}. Score: #{@movies.last.score}" 
-    puts "Average movie score: #{get_average_rating(@movies)}"
+    puts "Found: #{@movies.last.title}. Score: #{@movies.last.score} Year: #{@movies.last.year}" 
+    if @movies.length > 1
+      puts "Average movie score: #{get_average(@movies, "score", "float")}"
+      puts "Average movie year: #{get_average(@movies, "year", "integer")}"
+      puts get_satisfaction(@movies)
+    end
   end
 end
 
-def get_average_rating(movies)
-  all_ratings = 0
-  movies.each { |rating| all_ratings += rating.score }
-  all_ratings / movies.length.to_f
+def get_average(movies, attribute, type)
+  attribute_sum = 0
+  movies.each { |rating| attribute_sum += rating.send(attribute) }
+  if type == "float" 
+    return attribute_sum / movies.length.to_f
+  else
+    return attribute_sum / movies.length
+  end
 end
+
+def get_satisfaction(movies)
+  min_movie = movies.min_by(&:year)
+  max_movie = movies.max_by(&:year)
+  if (max_movie.score - min_movie.score).to_f / (max_movie.year - min_movie.year).to_f > 0
+    return "getting happier"
+  else
+    return "getting madder"
+  end
+end
+
+@movies = []
 
 find_movie
 
