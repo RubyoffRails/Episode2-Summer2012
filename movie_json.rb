a1d5c4b404721353ba69f29bc5da4cb99c5ac0d5
J. Michael Kasiewicz <crestcode@gmail.com>
2012-11-24 10:43:14 -0500

    Add error handling when searching for non-existant movies and keep a running average of movie ratings.

--- movie_json.rb
+++ movie_json.rb
@@ -2,16 +2,28 @@ require_relative "lib/movie"
 require_relative "lib/api"
 
 def find_movie
-  puts "OH HAI. Search?"
+  @movies = [] if @movies == nil
+  puts 'Add a movie you really like:'
   movie_title = gets
-  movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
+  if !(current_movie = Api.search_by_title(movie_title))
+    puts 'Oops. Something went wrong or title not found.'
+  else
+    @movies << current_movie
+    puts "Found: #{@movies.last.title}. Score: #{@movies.last.score}" 
+    puts "Average movie score: #{get_average_rating(@movies)}"
+  end
+end
+
+def get_average_rating(movies)
+  all_ratings = 0
+  movies.each { |rating| all_ratings += rating.score }
+  all_ratings / movies.length.to_f
 end
 
 find_movie
 
 while true do
-  puts "Search Again (Y/N)" 
+  puts 'Search Again (Y/N)'
   answer = gets.upcase[0]
   if answer == "Y"
     find_movie
