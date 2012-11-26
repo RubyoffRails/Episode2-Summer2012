fab8ee1c7ee7a8fb2e5070442e268e22fdda62e8
Ryan Moser <ryan_p_moser@yahoo.com>
2012-11-26 10:54:04 -0500

    Movie JSON - Tiger Level

--- lib/api.rb
+++ lib/api.rb
@@ -9,13 +9,20 @@ class Api
   def self.search_by_title(title)
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
     struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
-    Movie.new(id: struct.id.to_i,
-              title: struct.title,
-              year: struct.year,
-              score: struct.ratings["critics_score"]
-             )
+    if struct.title == nil
+      Movie.new(id: 1,
+                title: "No movie by that title",
+                year: 1111,
+                score: 0
+               )
+    else
+      Movie.new(id: struct.id.to_i,
+                title: struct.title,
+                year: struct.year,
+                score: struct.ratings["critics_score"]
+               )
+    end
   end
-
 
   def self.get_url_as_json(url)
     JSON.parse(open(url).read)
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -3,25 +3,45 @@ require "ostruct"
 
 describe Api do
 
-  let(:movie) { Api.search_by_title("Forrest Gump") }
+  describe "this is what a search that returns a movie should do" do
 
-  before do
-    Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
+
+    let(:movie) { Api.search_by_title("Forrest Gump") }
+
+    before do
+      Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
+    end
+
+    it "should search for movies" do
+      movie.title.should eq("Forrest Gump")
+    end
+
+    it "should return the score" do
+      movie.score.should eq(71)
+    end
+
+    it "should return the id" do
+      movie.id.should eq(10036)
+    end
+
+    it "should return the year" do
+      movie.year.should eq(1994)
+    end
+
   end
 
-  it "should search for movies" do
-    movie.title.should eq("Forrest Gump")
+  describe "this is how a search that does not find a movie should work" do
+
+    let(:movie) { Api.search_by_title("Noodle Weird") }
+
+    before do
+      Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/noodle.json")) }
+    end
+
+    it "should not crash if a search returns no movie" do
+      movie.title.should eq("No movie by that title")
+    end
+
   end
 
-  it "should return the score" do
-    movie.score.should eq(71)
-  end
-
-  it "should return the id" do
-    movie.id.should eq(10036)
-  end
-
-  it "should return the year" do
-    movie.year.should eq(1994)
-  end
 end
