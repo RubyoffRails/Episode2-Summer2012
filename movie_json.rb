d2f4b1494d491be3131c51dad6c4150836fe385a
Meshed <marksbrown@gmail.com>
2012-11-27 22:48:53 -0500

    Add average movie score

--- movie_json.rb
+++ movie_json.rb
@@ -1,13 +1,17 @@
 require_relative "lib/movie"
 require_relative "lib/api"
+require_relative "lib/search_history"
 
 def find_movie
   puts "Add a movie you really like"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
+  @search_history.add_search(movie)
   puts "Found: #{movie.title}. Score: #{movie.score}"
+  puts "Average movie score: #{@search_history.average_rating}."
 end
 
+@search_history = Search_History.new
 find_movie
 
 while true do
