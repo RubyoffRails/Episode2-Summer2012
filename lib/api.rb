1f6416a32828cd79109748728b5a5598bca50276
Meshed <marksbrown@gmail.com>
2012-11-28 22:02:19 -0500

    Use nil movie objects

--- lib/api.rb
+++ lib/api.rb
@@ -5,30 +5,23 @@ require_relative "./movie"
 class Api
   APIKEY="4t6456xa33z8qhcqyuqgnkjh"
 
+  def self.nil_movie
+    Movie.new(id: 0, title: "NOTHINGFOUNDHERE", year: 0, score: 0)
+  end
+
   def self.search_by_title(title)
-    if title != "" then
-      url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
-      struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
-      if struct.ratings.nil? == false then
-        movie = Movie.new(id: struct.id.to_i,
-                  title: struct.title,
-                  year: struct.year,
-                  score: struct.ratings["critics_score"]
-                 )
-        return movie
-      else
-        Movie.new(id: 0,
-                  title: "NOTHINGFOUNDHERE",
-                  year: 0,
-                  score: 0
-                 )
-      end
+    return nil_movie if title == ""
+    url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
+    struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
+    if struct.ratings.nil? == false then
+      movie = Movie.new(id: struct.id.to_i,
+                title: struct.title,
+                year: struct.year,
+                score: struct.ratings["critics_score"]
+               )
+      return movie
     else
-         Movie.new(id: 0,
-                  title: "NOTHINGFOUNDHERE",
-                  year: 0,
-                  score: 0
-                  )
+      return nil_movie
     end
   end
 
