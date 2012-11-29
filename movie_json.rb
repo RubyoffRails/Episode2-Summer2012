027b52f9ef9804a0772a120a762ad70735eb9d59
Anna Vester <veannaa@gmail.com>
2012-11-28 22:29:43 -0600

    Panda Level

--- movie_json.rb
+++ movie_json.rb
@@ -5,7 +5,13 @@ def find_movie
   puts "OH HAI. Search?"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
+
+  if movie == "Not Found"
+    puts "Not Found"
+  else
+    puts "Found: #{movie.title}. Score: #{movie.score}"
+  end
+
 end
 
 find_movie
