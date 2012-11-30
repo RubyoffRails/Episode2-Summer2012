283f70b1f01c53f94d12bedcc958753e72e9d478
Anna Vester <veannaa@gmail.com>
2012-11-29 22:17:43 -0600

    Tiger Level

--- movie_json.rb
+++ movie_json.rb
@@ -1,12 +1,24 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
-def find_movie
-  puts "OH HAI. Search?"
-  movie_title = gets
-  movie = Api.search_by_title(movie_title)
+def get_average(movies)
+  averages = []
 
-  puts "Found: #{movie.title}. Score: #{movie.score}" if !movie.nil?
+  movies.each do |m|
+    averages << m.score if !m.nil?
+  end
+
+  averages.inject{ |sum, el| sum + el }.to_f / averages.size
+
+end
+
+def find_movie
+  puts "Add a movie you really like"
+  movie_title = gets
+  @movies = [] if @movies.nil?
+  @movies << Api.search_by_title(movie_title)
+
+  puts "Found: #{@movies.last.title}. Score: #{@movies.last.score}. Avg. Score: #{get_average(@movies)}" if !@movies.last.nil?
 end
 
 find_movie
