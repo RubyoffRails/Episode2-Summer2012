dc199f301ac24f9f9b883640ae5dc23982ffd52a
Ralphos <wintle.ralph@gmail.com>
2012-12-09 19:32:12 +0800

    tests for context in which movie is not found

--- lib/api.rb
+++ lib/api.rb
@@ -16,7 +16,7 @@ class Api
                 score: struct.ratings["critics_score"]
                )
     rescue
-      nil
+      nil  
     end
   end
 
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -13,15 +13,32 @@ describe Api do
     movie.title.should eq("Forrest Gump")
   end
 
-  it "should return the score" do
-    movie.score.should eq(71)
+  context "when movie is found" do
+
+    it "should return the score" do
+      movie.score.should eq(71)
+    end
+
+    it "should return the id" do
+      movie.id.should eq(10036)
+    end
+
+    it "should return the year" do
+      movie.year.should eq(1994)
+    end
   end
 
-  it "should return the id" do
-    movie.id.should eq(10036)
+  context "when movie is not found" do
+
+    before do
+      Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/no_movie.json")) }
+    end
+    
+    let(:no_movie_found) { Api.search_by_title("No movies exists") }
+
+    it "doesn't raise an error" do
+      expect{:no_movie_found}.to_not raise_error(NoMethodError)
+    end
   end
 
-  it "should return the year" do
-    movie.year.should eq(1994)
-  end
 end
