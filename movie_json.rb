62a9dc7953653f4138bbb922702abbe136992fea
meltar <meltar@gmail.com>
2013-03-07 00:18:20 -0500

    tiger level complete

--- movie_json.rb
+++ movie_json.rb
@@ -1,23 +1,27 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
-def find_movie
-  puts "OH HAI. Search?"
-  movie_title = gets
-  movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
-rescue 
-  puts "Movie not found."
+def get_average_rating(movies)
+  scores = movies.collect { |i| i.score }
+  average = scores.inject(0.0) { |sum, rating| sum + rating } / scores.size
 end
 
-find_movie
+def find_movie(movies)
+  puts "Add a movie you really like."
+  movie_title = gets
+  movies << Api.search_by_title(movie_title)
+  puts "Found: #{movies.last.title}. Score: #{movies.last.score}"
+  puts "Average rating: #{get_average_rating(movies)}"
+rescue 
+ puts "Movie not found."
+end
 
-while true do
+movies = []
+while true
+  find_movie(movies)
   puts "Search Again (Y/N)" 
   answer = gets.upcase[0]
-  if answer == "Y"
-    find_movie
-  else
+  if answer != "Y"
     break
   end
 end
