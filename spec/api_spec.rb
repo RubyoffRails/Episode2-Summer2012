0b4b21490160168f1086fd63aaa2e70f6980297d
dharini <dramanat459@gmail.com>
2013-03-11 01:12:31 -0500

    fix test for invalid movie title

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -3,43 +3,46 @@ require "ostruct"
 
 describe Api do
 
-  describe "valid movie title" do
+  context "valid movie title" do
 
-  let(:movie) { Api.search_by_title("Forrest Gump") }
+    let(:movie) { Api.search_by_title("Forrest Gump") }
 
-    before do
-      expect {
-        Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
-      }.to_not raise_error
-    end
+      before do
+        expect {
+          Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
+        }.to_not raise_error
+      end
 
-    it "should search for movies" do
-      movie.title.should eq("Forrest Gump")
-    end
+      it "should search for movies" do
+        movie.title.should eq("Forrest Gump")
+      end
 
-    it "should return the score" do
-      movie.score.should eq(71)
-    end
+      it "should return the score" do
+        movie.score.should eq(71)
+      end
 
-    it "should return the id" do
-      movie.id.should eq(10036)
-    end
+      it "should return the id" do
+        movie.id.should eq(10036)
+      end
 
-    it "should return the year" do
-      movie.year.should eq(1994)
-    end
+      it "should return the year" do
+        movie.year.should eq(1994)
+      end
 
   end
 
-  describe "invalid movie title" do
+  context "invalid movie title" do
     let(:movie) { Api.search_by_title("hgh") }
 
-    it "should raise ArgumentError" do
-      expect {
-        Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/invalid.json")) }
-      }.to raise_error
-      movie.year.should eq(1994)
-    end
+      before do
+        expect {
+          Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/invalid.json")) }
+        }.to_not raise_error
+      end
+
+      it "should return :no_movie_found" do
+        movie.should eq(:no_movie_found)
+      end
 
   end
 
