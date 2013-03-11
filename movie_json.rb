f24689947f953a2f757269d24d0c50e6840fb68e
meltar <meltar@gmail.com>
2013-03-11 18:37:36 -0400

    eagle complete

--- movie_json.rb
+++ movie_json.rb
@@ -1,9 +1,26 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
-def get_average_rating(movies)
-  scores = movies.collect { |i| i.score }
-  average = scores.inject(0.0) { |sum, rating| sum + rating } / scores.size
+def get_average(movies, attribute)
+  if attribute == "score"
+    data = movies.collect { |i| i.score }
+  elsif attribute == "year"
+    data = movies.collect { |i| i.year }
+  else
+    data = []
+  end
+  average = data.inject(0.0) { |sum, rating| sum + rating } / data.size
+end
+
+def calculate_slope(movies)
+  movies = movies.sort_by { |i| i.year }
+  slope = (movies.last.score - movies.first.score).to_f / 
+            (movies.last.year - movies.first.year).to_f
+  if slope < 0 
+    puts "You are getting madder."
+  elsif slope > 0
+    puts "You are getting happier."
+  end
 end
 
 def find_movie
@@ -19,10 +36,11 @@ end
 movies = []
 while true
   movies << find_movie
-  puts "Average rating: #{get_average_rating(movies)}"
-  puts "Search Again (Y/N)" 
+  puts "Average rating: #{get_average(movies, "score")}. Average year: #{get_average(movies, "year")}"
+  puts "Search Again (Y/N)"
   answer = gets.upcase[0]
   if answer != "Y"
+    calculate_slope(movies) if movies.count > 1
     break
   end
 end
