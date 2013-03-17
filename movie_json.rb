634f28d4d11f2ff6be448eab4825ef9132f89f22
Darron Froese <darron@froese.org>
2013-03-17 14:53:29 -0600

    Eagle: Average Year.

--- movie_json.rb
+++ movie_json.rb
@@ -8,23 +8,39 @@ def find_movie
   movie_title = gets
   movie = Api.search_by_title(movie_title)
   unless movie.nil?
-    puts "Found: #{movie.title}. Score: #{movie.score}"
+    puts "Found: #{movie.title}. Score: #{movie.score}. Year: #{movie.year}"
     @movies << movie
     puts "\nMovies found so far: "
     movie_list(@movies)
+    average = average_ratings(@movies)
+    average_year = average_year(@movies)
+    puts "\nAverage Rating: #{average}"
+    puts "Average Year: #{average_year}"
   else
     puts "Movie not found\n"
   end
 end
 
 def movie_list(movies)
+  movies.each do |movie|
+    puts "#{movie.title} - #{movie.score} - #{movie.year}"
+  end
+end
+
+def average_ratings(movies)
   total = 0
   movies.each do |movie|
-    puts "#{movie.title} - #{movie.score}"
     total += movie.score
   end
   average = total / movies.length.to_i
-  puts "Average Rating: #{average} (#{total}/#{movies.length.to_i})"
+end
+
+def average_year(movies)
+  total = 0
+  movies.each do |movie|
+    total += movie.year
+  end
+  average_year = total / movies.length.to_i
 end
 
 find_movie
