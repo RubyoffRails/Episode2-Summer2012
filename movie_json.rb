7c66bba3b7085f6813554104ee99d4f240fe6041
Darron Froese <darron@froese.org>
2013-03-17 13:44:40 -0600

    Tiger Level

--- movie_json.rb
+++ movie_json.rb
@@ -1,21 +1,36 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
+@movies = []
+
 def find_movie
-  puts "OH HAI. Search?"
+  puts "Add a movie you really like:"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
   unless movie.nil?
     puts "Found: #{movie.title}. Score: #{movie.score}"
+    @movies << movie
+    puts "\nMovies found so far: "
+    movie_list(@movies)
   else
-    puts "Movie not found"
+    puts "Movie not found\n"
   end
+end
+
+def movie_list(movies)
+  total = 0
+  movies.each do |movie|
+    puts "#{movie.title} - #{movie.score}"
+    total += movie.score
+  end
+  average = total / movies.length.to_i
+  puts "Average Rating: #{average} (#{total}/#{movies.length.to_i})"
 end
 
 find_movie
 
 while true do
-  puts "Search Again (Y/N)" 
+  puts "\nSearch Again (Y/N)" 
   answer = gets.upcase[0]
   if answer == "Y"
     find_movie
