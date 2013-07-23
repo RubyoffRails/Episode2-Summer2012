de1c958659f429be6a666fb18d1e11d61293b98a
Loc Pru <yannai@live.com.mx>
2013-07-23 10:42:08 -0500

    Eagle level

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,6 +1,21 @@
 class Movie
 
   attr_reader :id, :title, :year, :score
+
+  class << self
+    attr_reader :movies
+  end
+
+  def self.add_movies(movie)
+    @movies ||= []
+    movies << movie
+  end
+
+  def self.average
+    total_score = movies.inject(0) { | sum, movie | sum + movie.score }
+    ( total_score / movies.length ).round
+  end
+
   def initialize(hash={})
     @id = hash.fetch(:id)
     @title = hash.fetch(:title)
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -8,5 +8,24 @@ describe Movie do
     movie.year.should eq(1998)
     movie.score.should eq(50)
   end
-  
+
+  context "Adding movies and operations with them" do
+    
+    before do
+      Movie.movies.clear if Movie.movies
+      movie1 = Movie.new(id: "the-id", title: "the-title", year: 1998, score: 50)
+      movie2 = Movie.new(id: 32      , title: "I'm 132"  , year: 2005, score: 100)
+      movie3 = Movie.new(id: 15500   , title: "Dogs"     , year: 2013, score: 88)
+      movies = [ movie1, movie2, movie3 ]
+      movies.each { | movie | Movie.add_movies(movie) }
+    end
+
+    it "should add movies the user likes" do
+      Movie.movies.length.should eq(3)
+    end
+
+    it "should calculate the average of the movies" do
+      Movie.average.should eq(79)
+    end
+  end
 end
