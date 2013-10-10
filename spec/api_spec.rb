603427309af9ee726c77a2cd9324489e3afe1111
Patil Varvarian <Patil.Varvarian@gmail.com>
2013-10-10 12:48:11 -0700

    I am done: P,T and E

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,27 +1,39 @@
 require_relative "../lib/api"
 require "ostruct"
 
-describe Api do
+describe Api do 
 
-  let(:movie) { Api.search_by_title("Forrest Gump") }
+	let(:movie) { Api.search_by_title("Forrest Gump") }
 
-  before do
-    Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
-  end
+	before do 
+		Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
+	end
 
-  it "should search for movies" do
-    movie.title.should eq("Forrest Gump")
-  end
+	it "should search for movies" do
+		movie = Api.search_by_title("Forrest Gump")
+		movie.title.should eq("Forrest Gump")
+	end
 
-  it "should return the score" do
-    movie.score.should eq(71)
-  end
+	it "should return the score" do
+		movie = Api.search_by_title("Forrest Gump")
+		movie.score.should eq(71)
+	end
 
-  it "should return the id" do
-    movie.id.should eq(10036)
-  end
+	it "should return the id" do
+		movie = Api.search_by_title("Forrest Gump")
+		movie.id.should eq(10036)
+	end
 
-  it "should return the year" do
-    movie.year.should eq(1994)
-  end
+
+	it "should return the year" do
+		movie = Api.search_by_title("Forrest Gump")
+		movie.year.should eq(1994)
+	end
+	
+	it "should return piggy when wrong movie" do
+		movie = Api.search_by_title("wofjewjfwpe")
+		movie.should eq(nil)
+	end
+
 end
+
