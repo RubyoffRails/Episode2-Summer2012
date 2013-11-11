c3bea8a8eee15d4f66658a2600ea2973ba2e3799
Han Loong Liauw <hanloongliauw@gmail.com>
2013-11-11 23:36:58 +1100

    finished tiger

--- movie_json.rb
+++ movie_json.rb
@@ -1,24 +1,29 @@
 require_relative "lib/movie"
+require_relative "lib/movie_library"
 require_relative "lib/api"
 
-def find_movie
-  puts "OH HAI. Search?"
+def find_movie library
+  puts "Add a movie you really like"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
   if movie
     puts "Found: #{movie.title}. Score: #{movie.score}"
+    library.add movie
+    puts "Current Average Score: #{library.average_score}"
   else
     puts "Nothing Found...."
+    false
   end
 end
 
-find_movie
+library = MovieLibrary.new
+find_movie library
 
 while true do
   puts "Search Again (Y/N)"
   answer = gets.upcase[0]
   if answer == "Y"
-    find_movie
+    find_movie library
   else
     break
   end
