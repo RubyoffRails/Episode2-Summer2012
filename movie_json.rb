a3cecf46751d68c05b65c442978bb9dc3d11351f
Ishan Thukral <dev@ishan.me>
2013-11-14 10:23:39 -0500

    finished eagle and tiger level

--- movie_json.rb
+++ movie_json.rb
@@ -1,21 +1,30 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
-def find_movie
-  puts "OH HAI. Search?"
+searched_movies = Array.new
+
+def find_movie (searched_movies)
+  puts "Add a movie you really like"
   movie_title = gets
   movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
+  if (movie)
+    puts "Found: #{movie.title}. Score: #{movie.score}"
+    searched_movies << movie.score
+  else
+    puts "Not Found"
+  end
 end
 
-find_movie
+find_movie (searched_movies)
 
 while true do
-  puts "Search Again (Y/N)" 
-  answer = gets.upcase[0]
-  if answer == "Y"
-    find_movie
-  else
+  puts "Add more? (Y/N)" 
+    answer = gets.upcase[0]
+    if answer == "Y"
+    find_movie (searched_movies)
+    else
+      raitng_average = searched_movies.inject{ |sum, el| sum + el }.to_f / searched_movies.size
+      puts "The average rating for your movies is: #{raitng_average}"
     break
-  end
+    end
 end
