efe58ebc85954139c48a61b4817efe17cadbfa24
Han Loong Liauw <hanloongliauw@gmail.com>
2013-11-16 15:59:34 +1100

    Tiger level, all green

--- movie_json.rb
+++ movie_json.rb
@@ -7,9 +7,22 @@ def find_movie library
   movie_title = gets
   movie = Api.search_by_title(movie_title)
   if movie
+
+    puts "\n"
     puts "Found: #{movie.title}. Score: #{movie.score}"
     library.add movie
+
+    puts "\n"
     puts "Current Average Score: #{library.average_score}"
+    puts "Current Average Year: #{library.average_year.to_i}"
+    puts "Current Slope: #{library.slope}"
+    if library.slope > 0
+      puts "Postive Slope: Getting Happier"
+    elsif library.slope < 0
+      puts "Negative Slope: Getting Sader"
+    else
+      puts "Your pretty balanced"
+    end
   else
     puts "Nothing Found...."
     false
@@ -20,6 +33,7 @@ library = MovieLibrary.new
 find_movie library
 
 while true do
+  puts "\n"
   puts "Search Again (Y/N)"
   answer = gets.upcase[0]
   if answer == "Y"
