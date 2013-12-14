db6bd2496086e81725e515e2d3de55a42c75e1a6
noahpatterson <noah@noah-patterson.com>
2013-12-14 16:53:05 -0500

    add .rspec, modify api.rb to simplify 'search_by_title', updated my version of movie_json.rb

--- lib/api.rb
+++ lib/api.rb
@@ -10,11 +10,7 @@ class Api
   def self.search_by_title(title)
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
     struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
-    Movie.new(id: struct.id.to_i,
-              title: struct.title,
-              year: struct.year,
-              score: struct.ratings["critics_score"]
-             )
+    store_movie(struct)
   end
 
 
@@ -22,4 +18,11 @@ class Api
     JSON.parse(open(url).read)
   end
 
+  def self.store_movie(movie_struct)
+    Movie.new(id: movie_struct.id.to_i,
+              title: movie_struct.title,
+              year: movie_struct.year,
+              score: movie_struct.ratings["critics_score"]
+             )
+  end
 end
--- movie_json.rb
+++ movie_json.rb
@@ -1,21 +1,50 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
-def find_movie
-  puts "OH HAI. Search?"
+def movie_search_by_title
+  puts "OH HAI. Please enter movie title"
+  print ">>" 
   movie_title = gets
-  movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
+  puts "Cool, searching for #{movie_title}"
+  search = Api.search_by_title(movie_title)
+  puts "#{movie_title} was released in #{search.year}. It recieved a critics score of #{search.score}"
 end
 
-find_movie
-
-while true do
-  puts "Search Again (Y/N)" 
-  answer = gets.upcase[0]
-  if answer == "Y"
-    find_movie
+def search_again(response)
+  if response == "yes"
+    movie_search_by_title
+  elsif response == "no"
+    false
   else
+    puts "I don't understand that"
+  end
+end
+
+movie_search_by_title
+
+while true
+  puts "Search Again?"
+  re_search = gets.downcase.chomp
+  if search_again(re_search) == false
     break
   end
 end
+
+# def find_movie
+#   puts "OH HAI. Search?"
+#   movie_title = gets
+#   movie = Api.search_by_title(movie_title)
+#   puts "Found: #{movie.title}. Score: #{movie.score}"
+# end
+
+# find_movie
+
+# while true do
+#   puts "Search Again (Y/N)" 
+#   answer = gets.upcase[0]
+#   if answer == "Y"
+#     find_movie
+#   else
+#     break
+#   end
+# end
