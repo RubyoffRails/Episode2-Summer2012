83a95622035004b14c63c4a64229014b05cfeae0
jason perez <jperezish@gmail.com>
2014-02-01 10:57:48 -0800

    fix search results to not throw an error if movie not found

--- lib/api.rb
+++ lib/api.rb
@@ -9,13 +9,20 @@ class Api
   def self.search_by_title(title)
     url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{APIKEY}&q=#{URI.encode(title)}&page_limit=1"
     struct = OpenStruct.new(get_url_as_json(url).fetch("movies").first)
-    Movie.new(id: struct.id.to_i,
-              title: struct.title,
-              year: struct.year,
-              score: struct.ratings["critics_score"]
-             )
+    if struct.respond_to?(:title)
+      Movie.new(id: struct.id.to_i,
+                title: struct.title,
+                year: struct.year,
+                score: struct.ratings["critics_score"]
+               )
+    else
+      Movie.new(id: 0,
+                title: "no results",
+                year: "",
+                score: "N/A"
+               )
+    end
   end
-
 
   def self.get_url_as_json(url)
     JSON.parse(open(url).read)
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -2,26 +2,37 @@ require_relative "../lib/api"
 require "ostruct"
 
 describe Api do
+  describe ".search_by_title" do
+    context "when title found" do
+      let(:movie) { Api.search_by_title("Forrest Gump") }
 
-  let(:movie) { Api.search_by_title("Forrest Gump") }
+      before do
+        Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
+      end
 
-  before do
-    Api.stub(:get_url_as_json) { JSON.parse(File.read("spec/fixtures/forrest.json")) }
-  end
+      it "should search for movies" do
+        movie.title.should eq("Forrest Gump")
+      end
 
-  it "should search for movies" do
-    movie.title.should eq("Forrest Gump")
-  end
+      it "should return the score" do
+        movie.score.should eq(71)
+      end
 
-  it "should return the score" do
-    movie.score.should eq(71)
-  end
+      it "should return the id" do
+        movie.id.should eq(10036)
+      end
 
-  it "should return the id" do
-    movie.id.should eq(10036)
-  end
+      it "should return the year" do
+        movie.year.should eq(1994)
+      end
+    end
 
-  it "should return the year" do
-    movie.year.should eq(1994)
+    context "when title not found" do
+      let(:movie) { Api.search_by_title("asdfasdfasdf") }
+
+      it "should not raise an error" do
+        movie.title.should_not raise_error
+      end
+    end
   end
 end
