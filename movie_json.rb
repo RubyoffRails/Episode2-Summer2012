c7a7953e6ac1338bb13d7dfdc8ca4bc72d91ce8a
drammopo <drammopo@gmail.com>
2014-02-19 23:21:03 +0200

    Added unrefactored Tiger Level.

--- movie_json.rb
+++ movie_json.rb
@@ -1,24 +1,32 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
+def average_movie_rating(movies_array)
+  movies_array.inject(0.0) { |sum, movie| sum + movie.score } / movies_array.size
+end
+
 def find_movie
   begin
-    puts "OH HAI. Search?"
+    puts "OH HAI. Add a movie you really like?"
     movie_title = gets
     movie = Api.search_by_title(movie_title)
-    puts "Found: #{movie.title}. Score: #{movie.score}"
+    @movies << movie
+    puts "Added: #{movie.title}. Score: #{movie.score}"
   rescue NoMethodError
     puts "Not found."
   end
 end
 
+@movies = []
 find_movie
+puts "The average rating is #{average_movie_rating(@movies)}."
 
 while true do
   puts "Search Again (Y/N)"
   answer = gets.upcase[0]
   if answer == "Y"
     find_movie
+    puts "The average rating is #{average_movie_rating(@movies)}."
   else
     break
   end
