a93af62ba9944827846ddcc0d11b7fc3252d2dad
drammopo <drammopo@gmail.com>
2014-02-25 18:03:58 +0200

    Refactored the use of instance variables and rename the catalog method.

--- lib/movie_library.rb
+++ lib/movie_library.rb
@@ -1,21 +1,22 @@
 class MovieLibrary
 
-  attr_reader :movies, :library, :slope
+  attr_reader :movies, :catalog, :slope
+  attr_accessor :slope
   def initialize
     @movies = []
-    @library = []
+    @catalog = []
     @slope = 0
   end
 
   def calculate_slope(catalog)
     first = catalog.first
     last = catalog.last
-    return 0 if first == last
-    (first.score - last.score).to_f / (last.year - first.year).to_f
+    return 0 if catalog.count <= 1
+    @slope = (first.score - last.score).to_f / (last.year - first.year).to_f
   end
 
   def sort_by_year(movies)
-    movies.sort { |movie,another_movie| movie.year <=> another_movie.year }
+    @catalog = movies.sort { |movie,another_movie| movie.year <=> another_movie.year }
   end
 
   def average_rating_by_year(year)
@@ -27,11 +28,10 @@ class MovieLibrary
     movies.inject(0.0) { |sum, movie| sum + movie.score } / movies.size
   end
 
-  def catalog(movie)
-    @movies << movie
-    @library = sort_by_year(@movies)
-    @slope = calculate_slope(@library)
-    return @library
+  def add(movie)
+    movies << movie
+    catalog = sort_by_year(movies)
+    calculate_slope(catalog)
   end
 
   def all_movies
--- spec/movie_library_spec.rb
+++ spec/movie_library_spec.rb
@@ -18,14 +18,14 @@ describe MovieLibrary do
     end
   end
 
-  describe "#catalog" do
+  describe "#add" do
 
     it 'should add a new movie to the library whenever a search happens' do
-      movie_library.catalog(movie)
+      movie_library.add(movie)
       expect(movie_library.all_movies).to include (movie)
     end
     it 'should increase the movie count' do
-      movie_library.catalog(movie)
+      movie_library.add(movie)
       movie_library.count.should eq(1)
     end
   end
@@ -37,7 +37,7 @@ describe MovieLibrary do
     end
 
     it 'should calculate the average rating' do
-      movie_library.catalog(movie)
+      movie_library.add(movie)
       movie_library.average_rating(movie_library.all_movies).should eq(50)
     end
   end
@@ -49,9 +49,9 @@ describe MovieLibrary do
     end
 
     it 'should calculate the average rating by year when the library has' do
-      movie_library.catalog(movie)
-      movie_library.catalog(another_movie)
-      movie_library.catalog(yet_another_movie)
+      movie_library.add(movie)
+      movie_library.add(another_movie)
+      movie_library.add(yet_another_movie)
       movie_library.average_rating_by_year(1998).should eq(50)
     end
   end
@@ -59,21 +59,21 @@ describe MovieLibrary do
   describe "#sort_by_year" do
 
     it 'should sort the movies by year' do
-      movie_library.catalog(another_movie)
-      movie_library.catalog(yet_another_movie)
-      movie_library.library.should == [yet_another_movie, another_movie]
+      movie_library.add(another_movie)
+      movie_library.add(yet_another_movie)
+      movie_library.catalog.should == [yet_another_movie, another_movie]
     end
   end
 
   describe "#calculate_slope" do
 
     it "should have no slope when the library only contains one movie" do
-      movie_library.catalog(movie)
+      movie_library.add(movie)
       movie_library.slope.should eq(0)
     end
     it 'should calculate the slope of the average ratings from the first year to the last year' do
-      movie_library.catalog(another_movie)
-      movie_library.catalog(yet_another_movie)
+      movie_library.add(another_movie)
+      movie_library.add(yet_another_movie)
       movie_library.slope.should eq(-0.22727272727272727)
     end
 
